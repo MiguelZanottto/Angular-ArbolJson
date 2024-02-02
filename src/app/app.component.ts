cd7e9ed5b7f0ea6d9f3f4b44fb7dd5c7b93ea8b1
MiguelZanottto <migzanotto18@hotmail.com>
2024-02-02 18:01:18 +0100

    Entrega

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'arbol';
+  comunidades: any;
+  comunidadSelected: any;
+  provinciaSelected: any;
+  localidadSelected: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get("/assets/arbol.json").subscribe(
+      (resultado: any) => {
+        this.comunidades = resultado;
+      }
+    );
+  }
+
+  onComunidadChange() {
+    this.provinciaSelected = null; 
+    this.localidadSelected = null; 
+  }
+
+  onProvinciaChange() {
+    this.localidadSelected = null;
+  }
 }
